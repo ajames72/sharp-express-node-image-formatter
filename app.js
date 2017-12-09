d428b13fe1a7a887ef530d2b5c7702210f12fc3f
ajames72 <andrew.james@ajmebc.co.uk>
2017-12-09 09:12:21 +0000

    FIxed templating engine
    Using jade by default, switched to node/express dot

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var engine = require('express-dot-engine');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -11,8 +12,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('dot', engine.__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'dot');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
